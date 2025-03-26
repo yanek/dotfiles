1b2308ae81c8afe5e301a57cf64ced7d69937466
Noé Ksiazek <noe.ksiazek@pm.me>
2025-03-26 17:04:47 +0100

    reduce wezterm background opacity

--- neovim/.config/nvim/lua/options.lua
+++ neovim/.config/nvim/lua/options.lua
@@ -4,4 +4,12 @@ local o = vim.o
 
 o.wrap = false
 o.relativenumber = true
-o.cursorlineopt = 'both'
+o.cursorlineopt = "both"
+
+-- Remove background
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,6 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.enable_tab_bar = false
-config.window_background_opacity = 1
+config.window_background_opacity = 0.95
 
 return config
