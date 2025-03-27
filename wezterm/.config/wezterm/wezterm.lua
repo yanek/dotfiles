30952ece9ae0f33e006489e9a70fbb0165d38bec
Noé Ksiazek <noe.ksiazek@pm.me>
2025-03-27 11:17:26 +0100

    update global theme to nord

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.color_scheme = 'nord'
 config.font_size = 11
 config.enable_tab_bar = false
 config.window_background_opacity = 0.98
