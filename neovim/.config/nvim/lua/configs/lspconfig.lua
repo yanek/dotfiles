c22a3622f1feb2796e5c8b4b1fae25fe2a66c4a1
Noé Ksiazek <noe.ksiazek@pm.me>
2025-03-27 02:48:00 +0100

    add helix, configure glsl lsp, change wezterm font size

--- neovim/.config/nvim/lua/configs/lspconfig.lua
+++ neovim/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ols" }
+local servers = { "html", "cssls", "ols", "glsl_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.font_size = 11
 config.enable_tab_bar = false
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.98
 
 return config
