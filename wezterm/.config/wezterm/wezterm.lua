f770aa729b0aa11a88f2b911f8c629015492a2e3
Noé Ksiazek <noe.ksiazek@pm.me>
2025-03-28 10:38:32 +0100

    wezterm: change default terminal size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,5 +5,7 @@ config.color_scheme = 'nord'
 config.font_size = 11
 config.enable_tab_bar = false
 config.window_background_opacity = 0.98
+config.initial_cols = 128
+config.initial_rows = 32
 
 return config
