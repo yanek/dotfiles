848390187eadfbc6b9b199afcd5ba31046ec8e4a
Noé Ksiazek <noe.ksiazek@pm.me>
2025-03-31 22:07:50 +0200

    wezterm: improve font rendering (on my display)

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,11 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'nord'
-config.font_size = 11
+config.freetype_load_flags = "NO_HINTING"
+config.freetype_load_target = "HorizontalLcd"
+config.font_size = 10
+config.line_height = 1.1
+config.font = wezterm.font {family = "IBM Plex Mono", weight = "DemiBold"}
 config.enable_tab_bar = false
 config.window_background_opacity = 0.98
 config.initial_cols = 128
