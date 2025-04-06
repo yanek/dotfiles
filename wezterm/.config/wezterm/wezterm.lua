00bf797ecb4137e6370fabc036c64e48094cdf09
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-06 12:40:35 +0200

    wezterm: update, remove title bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,9 +7,19 @@ config.freetype_load_target = "HorizontalLcd"
 config.font_size = 10
 config.line_height = 1.1
 config.font = wezterm.font {family = "IBM Plex Mono", weight = "DemiBold"}
-config.enable_tab_bar = false
-config.window_background_opacity = 0.98
-config.initial_cols = 160
-config.initial_rows = 32
+config.max_fps = 120
+
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_opacity = 1
+config.initial_cols = 180
+config.initial_rows = 40
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 return config
