1021f743380d429fbd6dd29b5f8b52d15e826fcb
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-12 02:28:19 +0200

    wezterm: update visual config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,12 +2,17 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'nord'
-config.freetype_load_flags = "NO_HINTING"
-config.freetype_load_target = "HorizontalLcd"
-config.font_size = 10
+-- config.freetype_load_flags = "NO_HINTING"
+-- config.freetype_load_target = "HorizontalLcd"
+config.font_size = 11
 config.line_height = 1.1
-config.font = wezterm.font {family = "IBM Plex Mono", weight = "DemiBold"}
+config.font = wezterm.font {family = "Fira Code", weight = "Medium"}
 config.max_fps = 120
+config.default_cursor_style = "BlinkingBar"
+config.cursor_thickness = 2
+config.cursor_blink_rate = 800
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
@@ -16,10 +21,10 @@ config.window_background_opacity = 1
 config.initial_cols = 180
 config.initial_rows = 40
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+  left = 8,
+  right = 8,
+  top = 8,
+  bottom = 8,
 }
 
 return config
